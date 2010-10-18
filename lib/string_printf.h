388e3835286c3f0fa9c735416281eaf713a5cef8
unknown <ctaylor@.sparkfun.local>
2010-10-18 14:05:29 -0600

    ASCII logging at 115200 baud drops packets about every 176144 bytes.

---
+++ lib/string_printf.h
@@ -0,0 +1,4 @@
+
+extern void string_myputchar(char* string_printf_buffer, unsigned char c);
+extern void string_printf_devopen( int(*put)(int) );
+extern void string_printf(char* string_printf_buffer, char const *format, ... );
